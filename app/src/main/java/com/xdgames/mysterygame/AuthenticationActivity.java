91ba85784b5d13afb543a448f63742d41bd3ebdd
whatXianheCodes <xianhehuang@gmail.com>
2014-12-01 22:33:57 -0800

    finished checks will TODO error messages

--- app/src/main/java/com/xdgames/mysterygame/AuthenticationActivity.java
+++ app/src/main/java/com/xdgames/mysterygame/AuthenticationActivity.java
@@ -47,26 +47,31 @@ public class AuthenticationActivity extends ActionBarActivity {
         //TODO: add security check to determine whether the code is valid
         EditText securityCodeView = (EditText)findViewById(R.id.invitation_code);
         String securityCodeValue = securityCodeView.getText().toString();
-        final String INVITE_CODE = "....-....-....-....$";
-        Pattern r = Pattern.compile(INVITE_CODE);
-
-        Matcher m = r.matcher(securityCodeValue);
-        return m.find();
+        final String INVITE_CODE_PATTERN = "^[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}$";
+        return securityCodeValue.matches(INVITE_CODE_PATTERN);
     }
 
     private boolean checkName () {
-        return true;
+        EditText firstNameView = (EditText)findViewById(R.id.first_name);
+        String firstNameValue = firstNameView.getText().toString();
+        EditText lastNameView = (EditText) findViewById(R.id.last_name);
+        String lastNameValue =  lastNameView.getText().toString();
+        final String NAME_PATTERN = "[-A-Za-z]{1,}";
+        return (firstNameValue.matches(NAME_PATTERN) && lastNameValue.matches(NAME_PATTERN));
     }
 
     private boolean checkEmail () {
-        EditText emailView = (EditText)findViewById(R.id.invitation_code);
+        EditText emailView = (EditText)findViewById(R.id.email);
         String emailValue = emailView.getText().toString();
-        final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\\\+]+(\\\\.[_A-Za-z0-9-]+)*@\"\n" +
-                "\t\t+ \"[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$";
-        Pattern r = Pattern.compile(EMAIL_PATTERN);
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        return emailValue.matches(EMAIL_PATTERN);
+    }
 
-        Matcher m = r.matcher(emailValue);
-        return m.find();
+    private boolean checkPassword () {
+        EditText passwordView = (EditText)findViewById(R.id.password);
+        String passwordValue = passwordView.getText().toString();
+        return (passwordValue.length()!= 0);
     }
 
     public void submitForm (View view) {
@@ -77,6 +82,9 @@ public class AuthenticationActivity extends ActionBarActivity {
             return;
         }
         else if (!checkEmail()) {
+            return;
+        }
+        else if (!checkPassword()){
             return;
         }
 
