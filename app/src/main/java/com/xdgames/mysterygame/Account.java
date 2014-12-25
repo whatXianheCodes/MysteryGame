846348f2493f7dea5073b5c3cba6c9e2260681f9
whatXianheCodes <xianhehuang@gmail.com>
2014-12-25 17:21:38 -0500

    refactor to make it more organized

---
+++ app/src/main/java/com/xdgames/mysterygame/Account.java
@@ -0,0 +1,100 @@
+package com.xdgames.mysterygame;
+
+/**
+ * Created by Xianhe on 12/25/2014.
+ */
+public class Account {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String invitationCode;
+
+    public Account (String firstName, String lastName, String username,
+                    String password, String email, String invitationCode ){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.invitationCode = invitationCode;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public String getFirstName () {
+        return this.firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setUsername (String username) {
+        this.username = username;
+    }
+
+    public String getUsername () {
+        return this.username;
+    }
+
+    public void setPassword (String password) {
+        this.password = password;
+    }
+
+    public String getPassword () {
+        return this.password;
+    }
+
+    public void setInvitationCode(String invitationCode) {
+        this.invitationCode = invitationCode;
+    }
+
+    public String getInvitationCode() {
+        return this.invitationCode;
+    }
+
+    public void setEmail (String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public boolean checkInvitation () {
+        //TODO: add security check to determine whether the code is valid
+        final String INVITE_CODE_PATTERN = "^[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}$";
+        return this.invitationCode.matches(INVITE_CODE_PATTERN);
+    }
+
+    public boolean checkName () {
+        final String NAME_PATTERN = "[-A-Za-z]{1,}";
+        return (this.firstName.matches(NAME_PATTERN) && this.lastName.matches(NAME_PATTERN));
+    }
+
+    public boolean checkEmail () {
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        return this.email.matches(EMAIL_PATTERN);
+    }
+
+    public boolean checkPassword () {
+        return (this.password.length()!= 0);
+    }
+
+    public boolean checkConfirmPassword (String passwordConfirmValue) {
+        return (this.password.equals(passwordConfirmValue));
+    }
+
+    public boolean checkUsername () {
+        return (this.username.length() != 0);
+    }
+}
--- app/src/main/java/com/xdgames/mysterygame/AuthenticationActivity.java
+++ app/src/main/java/com/xdgames/mysterygame/AuthenticationActivity.java
@@ -36,13 +36,16 @@ public class AuthenticationActivity extends ActionBarActivity {
     public final static String EXTRA_EMAIL = "com.xdgames.mystergygame.EMAIL";
     public final static String EXTRA_PASSWORD = "com.xdgames.mysterygame.PASSWORD";
     public final static String EXTRA_USERNAME = "com.xdgames.mysterygame.USERNAME";
+
     private String apiEndpoint = "http://xdgames.xianheh.com/MysteryGame";
+    private Account account;
+    private JSONObject registrationJSON;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,100 +69,34 @@ public class AuthenticationActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private boolean checkInvitation (String securityCodeValue) {
-        //TODO: add security check to determine whether the code is valid
-        final String INVITE_CODE_PATTERN = "^[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}-[A-Za-z0-9]{4}$";
-        return securityCodeValue.matches(INVITE_CODE_PATTERN);
-    }
-
-    private boolean checkName (String firstNameValue, String lastNameValue) {
-        final String NAME_PATTERN = "[-A-Za-z]{1,}";
-        return (firstNameValue.matches(NAME_PATTERN) && lastNameValue.matches(NAME_PATTERN));
-    }
-
-    private boolean checkEmail (String emailValue) {
-        final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-        return emailValue.matches(EMAIL_PATTERN);
-    }
-
-    private boolean checkPassword (String passwordValue) {
-        return (passwordValue.length()!= 0);
-    }
-
-    private boolean checkConfirmPassword (String passwordValue, String passwordConfirmValue) {
-        return (passwordValue.equals(passwordConfirmValue));
-    }
-
-    private boolean checkUsername (String usernameValue) {
-        // TODO: check database if username isn't taken. Need database first
-        return (usernameValue.length() != 0);
-    }
-    private void displayInvalidMessageToast (String message) {
+    private void displayToastMessage (String message) {
         Toast toast = Toast.makeText(this, message, Toast.LENGTH_SHORT);
         toast.show();
     }
 
-    public void submitForm (View view) {
-        EditText invitationCodeView = (EditText)findViewById(R.id.invitation_code);
-        String invitationCodeValue = invitationCodeView.getText().toString();
-        EditText firstNameView = (EditText)findViewById(R.id.first_name);
-        String firstNameValue = firstNameView.getText().toString();
-        EditText lastNameView = (EditText) findViewById(R.id.last_name);
-        String lastNameValue =  lastNameView.getText().toString();
-        EditText emailView = (EditText)findViewById(R.id.email);
-        String emailValue = emailView.getText().toString();
-        EditText passwordView = (EditText)findViewById(R.id.password);
-        String passwordValue = passwordView.getText().toString();
-        EditText passwordConfirmView = (EditText)findViewById(R.id.confirm_password);
-        String passwordConfirmValue = passwordView.getText().toString();
-        EditText usernameView = (EditText)findViewById(R.id.username);
-        String usernameValue = usernameView.getText().toString();
-        /*if (!checkName(firstNameValue, lastNameValue)) {
-            displayInvalidMessageToast(getResources().getString(R.string.invalid_name_message));
-            return;
-        }
-        else if (!checkUsername(usernameValue)) {
-            displayInvalidMessageToast(getResources().getString(R.string.invalid_username_message));
-            return;
-        }
-        else if (!checkPassword(passwordValue)){
-            displayInvalidMessageToast(getResources().getString(R.string.invalid_password_message));
-            return;
-        }
-        else if (!checkConfirmPassword(passwordValue, passwordConfirmValue)){
-            displayInvalidMessageToast(getResources().getString(R.string.invalid_password_confirm_message));
-            return;
-        }
-        else if (!checkEmail(emailValue)) {
-            displayInvalidMessageToast(getResources().getString(R.string.invalid_email_message));
-            return;
-        }
-        else if (!checkInvitation(invitationCodeValue)) {
-            displayInvalidMessageToast(getResources().getString(R.string.invalid_invitation_message));
-            return;
-        }*/
-        final JSONObject jsonRegistration = new JSONObject();
-        JSONObject jsonUser = new JSONObject();
+    private void createRegistrationJson () {
+        registrationJSON = new JSONObject();
+        JSONObject userJSON = new JSONObject();
         try {
-            jsonUser.put("firstname", firstNameValue);
-            jsonUser.put("lastname", lastNameValue);
-            jsonUser.put("username", usernameValue);
-            jsonUser.put("password", passwordValue);
-            jsonUser.put("email", emailValue);
-            jsonUser.put("invitation_code", invitationCodeValue);
-            jsonRegistration.put("registration", jsonUser);
+            userJSON.put("firstname", account.getFirstName());
+            userJSON.put("lastname", account.getLastName());
+            userJSON.put("username", account.getUsername());
+            userJSON.put("password", account.getPassword());
+            userJSON.put("email", account.getEmail());
+            userJSON.put("invitation_code", account.getInvitationCode());
+            registrationJSON.put("registration", userJSON);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+    private void sendRegistration () {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 HttpPost httpPost = new HttpPost(apiEndpoint);
                 httpPost.setHeader("Content-Type", "application/json");
                 try {
-                    httpPost.setEntity(new StringEntity(jsonRegistration.toString()));
-                    Log.d(TAG, jsonRegistration.toString());
+                    httpPost.setEntity(new StringEntity(registrationJSON.toString()));
                     HttpParams httpParams = new BasicHttpParams();
                     HttpConnectionParams.setConnectionTimeout(httpParams, 10* 1000);
                     HttpConnectionParams.setSoTimeout(httpParams, 10*1000);
@@ -171,14 +108,69 @@ public class AuthenticationActivity extends ActionBarActivity {
                 }
             }
         }).start();
+    }
 
-//        return intent = new Intent(this, MainActivity.class);
-//        intent.putExtra(EXTRA_INVITATION, securityCodeValue);
-//        intent.putExtra(EXTRA_USERNAME, usernameValue);
-//        intent.putExtra(EXTRA_FNAME, firstNameValue);
-//        intent.putExtra(EXTRA_LNAME, lastNameValue);
-//        intent.putExtra(EXTRA_EMAIL, emailValue);
-//        intent.putExtra(EXTRA_PASSWORD, passwordValue);
-        //startActivity(intent);
+    private void handleRegistrationResponse (HttpResponse response) {
+        if (response.getStatusLine().getStatusCode() == 200) {
+            displayToastMessage("Login Successed");
+        }
+        else {
+            displayToastMessage("Login Failed");
+        }
+    }
+
+    private boolean checkValidForm () {
+        EditText passwordConfirmView = (EditText)findViewById(R.id.confirm_password);
+        String passwordConfirmValue = passwordConfirmView.getText().toString();
+        if (!account.checkName()) {
+            displayToastMessage(getResources().getString(R.string.invalid_name_message));
+            return false;
+        }
+        else if (!account.checkUsername()) {
+            displayToastMessage(getResources().getString(R.string.invalid_username_message));
+            return false;
+        }
+        else if (!account.checkPassword()){
+            displayToastMessage(getResources().getString(R.string.invalid_password_message));
+            return false;
+        }
+        else if (!account.checkConfirmPassword(passwordConfirmValue)){
+            displayToastMessage(getResources().getString(R.string.invalid_password_confirm_message));
+            return false;
+        }
+        else if (!account.checkEmail()) {
+            displayToastMessage(getResources().getString(R.string.invalid_email_message));
+            return false;
+        }
+        else if (!account.checkInvitation()) {
+            displayToastMessage(getResources().getString(R.string.invalid_invitation_message));
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    public void submitForm (View view) {
+        EditText invitationCodeView = (EditText)findViewById(R.id.invitation_code);
+        String invitationCodeValue = invitationCodeView.getText().toString();
+        EditText firstNameView = (EditText)findViewById(R.id.first_name);
+        String firstNameValue = firstNameView.getText().toString();
+        EditText lastNameView = (EditText) findViewById(R.id.last_name);
+        String lastNameValue =  lastNameView.getText().toString();
+        EditText emailView = (EditText)findViewById(R.id.email);
+        String emailValue = emailView.getText().toString();
+        EditText passwordView = (EditText)findViewById(R.id.password);
+        String passwordValue = passwordView.getText().toString();
+        EditText usernameView = (EditText)findViewById(R.id.username);
+        String usernameValue = usernameView.getText().toString();
+
+        account = new Account (firstNameValue, lastNameValue, usernameValue,
+                passwordValue, emailValue, invitationCodeValue);
+
+//        if (!checkValidForm()) {
+//            return;
+//        }
+        createRegistrationJson();
+        sendRegistration();
     }
 }
