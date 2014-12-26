ecb50321a8008b15d3b9ac9a6d97a1647c38d300
whatXianheCodes <xianhehuang@gmail.com>
2014-12-26 13:56:57 -0500

    add dialog will add detail more

--- app/src/main/java/com/xdgames/mysterygame/Utils/RegistrationRequest.java
+++ app/src/main/java/com/xdgames/mysterygame/Utils/RegistrationRequest.java
@@ -1,11 +1,15 @@
 package com.xdgames.mysterygame.Utils;
 
+import android.app.AlertDialog;
+import android.app.Fragment;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.xdgames.mysterygame.Models.Account;
+import com.xdgames.mysterygame.UI.ProgressDialogFragment;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
@@ -22,7 +26,7 @@ import java.io.IOException;
 /**
  * Created by Xianhe on 12/25/2014.
  */
-public class RegistrationRequest extends AsyncTask <Account, Integer, String>{
+public class RegistrationRequest extends AsyncTask <Account, Void, String>{
 
     private static final String TAG = "RegistrationRequest";
 
@@ -62,14 +66,14 @@ public class RegistrationRequest extends AsyncTask <Account, Integer, String>{
         }
     }
 
-     @Override
+    @Override
     protected String doInBackground(Account... params) {
+         publishProgress();
          createRegistrationJson(params[0]);
          HttpPost httpPost = new HttpPost(apiEndpoint);
          httpPost.setHeader("Content-Type", "application/json");
          try {
              httpPost.setEntity(new StringEntity(registrationJSON.toString()));
-             Log.d(TAG, registrationJSON.toString());
              HttpParams httpParams = new BasicHttpParams();
              HttpConnectionParams.setConnectionTimeout(httpParams, 10 * 1000);
              HttpConnectionParams.setSoTimeout(httpParams, 10*1000);
@@ -81,6 +85,24 @@ public class RegistrationRequest extends AsyncTask <Account, Integer, String>{
              e.printStackTrace();
              return "You have failed to create an account.";
          }
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+        Log.d(TAG, "testing...");
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("This is a message")
+                .setTitle("Alert")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d(TAG, "Ok Pressed");
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 
     @Override
